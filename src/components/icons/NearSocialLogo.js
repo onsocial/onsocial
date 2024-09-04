0f46f65e9c3021838c72fd73fdb62831067b2237
green ghost <143076132+greenghostnear@users.noreply.github.com>
2024-09-04 22:44:01 +0100

    Update NearSocialLogo.js

--- src/components/icons/NearSocialLogo.js
+++ src/components/icons/NearSocialLogo.js
@@ -3,4 +3,4 @@ import React from "react";
 export function NearSocialLogo({    }) {
   return (
    
-    <img src="https://app.vfdao.org/app.png" alt="Logotype" width="50" height="100" class="logotype" />);}
+    <img src="https://app.vfdao.org/app.png" alt="Logotype" width="70" height="70" class="logotype" />);}
