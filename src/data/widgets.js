3bafdc34f1587c0b34442323dadcc9c2d0e93c0a
green ghost <143076132+greenghostnear@users.noreply.github.com>
2024-09-18 21:40:43 +0100

    Update widgets.js

--- src/data/widgets.js
+++ src/data/widgets.js
@@ -25,7 +25,7 @@ const MainnetWidgets = {
   widgetMetadataEditor: "mob.near/widget/WidgetMetadataEditor",
   widgetMetadata: "mob.near/widget/WidgetMetadata",
   profileImage: "mob.near/widget/ProfileImage",
-  notificationButton: "mob.near/widget/NotificationButton",
+  notificationButton: "vfdao.near/widget/NotificationButton",
   profilePage: "vfdao.near/widget/ProfilePage",
   profileName: "patrick.near/widget/ProfileName",
   editorComponentSearch: "mob.near/widget/Editor.ComponentSearch",
