6262b2409d9045270009b84325ef64b82655d86c
green ghost <143076132+greenghostnear@users.noreply.github.com>
2024-12-03 16:12:11 +0000

    Update DesktopNavigation.js

--- src/components/navigation/desktop/DesktopNavigation.js
+++ src/components/navigation/desktop/DesktopNavigation.js
@@ -75,7 +75,7 @@ export function DesktopNavigation(props) {
         </Link>
         <div className="navigation-section">
           <NavigationButton route="/">Home</NavigationButton>
- <NavigationButton route="/veganfriends.near/widget/donate-to">Charity</NavigationButton>
+ <NavigationButton route="/vfdao.near/widget/Charities.Group</NavigationButton>
 
           <NavigationButton href={props.documentationHref}>
             VF Labs
