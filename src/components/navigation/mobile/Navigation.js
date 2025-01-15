c3205863b213d1ac14efdfe5eaf2e258963f0eb3
green ghost <143076132+greenghostnear@users.noreply.github.com>
2025-01-15 03:52:54 +0000

    Adding post button

--- src/components/navigation/mobile/Navigation.js
+++ src/components/navigation/mobile/Navigation.js
@@ -11,6 +11,7 @@ import { Search } from "../../icons/Search";
 import { Home2 } from "../../icons/Home2";
 import { Ai } from "../../icons/Ai";
 import { Dots } from "../../icons/Dots";
+import { PostButton } from "../../icons/PostButton";
 
 const StyledNavigation = styled.div`
   position: sticky;
@@ -79,9 +80,9 @@ const IconContainer = styled.div`
 
 const StyledFloatingSVG = styled.div`
   position: fixed;
-  bottom: 74px;
-  right: 24px;
-  z-index: 1100; /* Above other elements */
+  bottom: 64px;
+  right: 14px;
+  z-index: 1;
   width: 60px;
   height: 60px;
   cursor: pointer;
@@ -192,29 +193,13 @@ export function Navigation(props) {
         </IconContainer>
       </StyledBottomNavigation>
 
-      <StyledFloatingSVG isVisible={showSVG}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="120 120 60 60"
-          style={{ width: "60px", height: "60px" }}
-        >
-          <g>
-            <ellipse
-              rx="30"
-              ry="30"
-              transform="translate(150 150)"
-              fill="#82e299"
-              strokeWidth="0"
-            />
-          </g>
-          <g transform="matrix(.819758 0 0 0.819758 135.351745 135.351542)">
-            <path
-              d="M0,35.667c0,0,11.596-37.07,35.738-35.55c0,0-2.994,4.849-10.551,6.416c0,0,3.518.429,6.369-.522c0,0-1.711,5.515-11.025,6.273c0,0,5.133,1.331,7.414.57c0,0-.619,4.111-10.102,6.154-.562.12-4.347,1.067-1.306,1.448c0,0,4.371.763,5.514.381c0,0-3.744,5.607-12.928,5.132-.903-.047-1.332,0-1.332,0L0,35.667Z"
-              fill="#fff"
-            />
-          </g>
-        </svg>
-      </StyledFloatingSVG>
+     <StyledFloatingSVG isVisible={showSVG}>
+  <Link to="/onsocial.near/widget/MainPage.N.Compose">
+    <PostButton />
+  </Link>
+</StyledFloatingSVG>
+
+
     </>
   );
 }
