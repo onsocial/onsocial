74856b2e34375e771daa9094d4a91ccdb8780ee0
green ghost <143076132+greenghostnear@users.noreply.github.com>
2025-02-09 19:41:48 +0000

    Update UserCircle.js

--- src/components/icons/UserCircle.js
+++ src/components/icons/UserCircle.js
@@ -2,34 +2,9 @@ import React from "react";
 
 export function UserCircle() {
   return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M19.25 12C19.25 16.0041 16.0041 19.25 12 19.25C7.99594 19.25 4.75 16.0041 4.75 12C4.75 7.99594 7.99594 4.75 12 4.75C16.0041 4.75 19.25 7.99594 19.25 12Z"
-        stroke="#232528"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M14.25 10C14.25 11.2426 13.2426 12.25 12 12.25C10.7574 12.25 9.75 11.2426 9.75 10C9.75 8.75736 10.7574 7.75 12 7.75C13.2426 7.75 14.25 8.75736 14.25 10Z"
-        stroke="#232528"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M7.19745 17C8.34392 15.625 10.0698 14.75 12 14.75C13.9302 14.75 15.6561 15.625 16.8025 17"
-        stroke="#232528"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
+    <svg width="24px" height="24px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" version="1.1" fill="none" stroke="#232528" stroke-linecap="round" stroke-linejoin="round" stroke-width="1">
+<path d="M8 9.25 A3.25 3.25 0 1 1 8 2.75 A3.25 3.25 0 1 1 8 9.25"/>
+<path d="m2.75 14.25c0-2.5 2-5 5.25-5s5.25 2.5 5.25 5"/>
+</svg>
   );
 }
